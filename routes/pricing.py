d39aed24857ba9e94a944402b5ee30a4bdfdc37a
Vestenkjaer <vestenkjaer@gmail.com>
2024-07-08 08:59:50 +0200

    Delete routes directory

--- routes/pricing.py
+++
@@ -1,8 +0,0 @@
-# routes/pricing.py
-from flask import Blueprint, render_template
-
-pricing_bp = Blueprint('pricing', __name__)
-
-@pricing_bp.route('/')
-def pricing():
-    return render_template('pricing.html')
