8df92a512b776d3598f9a9968fc2ac04123c7ad5
Rater <support@raterware.com>
2024-07-08 09:19:23 +0200

    content update

--- set_heroku_env.bat
+++ set_heroku_env.bat
@@ -10,17 +10,51 @@ for /f "usebackq tokens=*" %%a in (.env) do (
     )
 )
 
-:: Set environment variables on Heroku
+:: Debug statements to verify variable values
+echo MAIL_SERVER=%MAIL_SERVER%
+echo MAIL_PORT=%MAIL_PORT%
+echo MAIL_USERNAME=%MAIL_USERNAME%
+echo MAIL_PASSWORD=%MAIL_PASSWORD%
+echo MAIL_DEFAULT_SENDER=%MAIL_DEFAULT_SENDER%
+echo AUTH0_CLIENT_ID=%AUTH0_CLIENT_ID%
+echo AUTH0_CLIENT_SECRET=%AUTH0_CLIENT_SECRET%
+echo AUTH0_DOMAIN=%AUTH0_DOMAIN%
+echo AUTH0_CALLBACK_URL=%AUTH0_CALLBACK_URL%
+echo STRIPE_SECRET_KEY=%STRIPE_SECRET_KEY%
+echo STRIPE_PUBLISHABLE_KEY=%STRIPE_PUBLISHABLE_KEY%
+
+:: Set environment variables on Heroku one by one
+echo Setting MAIL_SERVER on Heroku
 heroku config:set MAIL_SERVER=%MAIL_SERVER% -a raterware
+
+echo Setting MAIL_PORT on Heroku
 heroku config:set MAIL_PORT=%MAIL_PORT% -a raterware
+
+echo Setting MAIL_USERNAME on Heroku
 heroku config:set MAIL_USERNAME=%MAIL_USERNAME% -a raterware
+
+echo Setting MAIL_PASSWORD on Heroku
 heroku config:set MAIL_PASSWORD=%MAIL_PASSWORD% -a raterware
+
+echo Setting MAIL_DEFAULT_SENDER on Heroku
 heroku config:set MAIL_DEFAULT_SENDER=%MAIL_DEFAULT_SENDER% -a raterware
+
+echo Setting AUTH0_CLIENT_ID on Heroku
 heroku config:set AUTH0_CLIENT_ID=%AUTH0_CLIENT_ID% -a raterware
+
+echo Setting AUTH0_CLIENT_SECRET on Heroku
 heroku config:set AUTH0_CLIENT_SECRET=%AUTH0_CLIENT_SECRET% -a raterware
+
+echo Setting AUTH0_DOMAIN on Heroku
 heroku config:set AUTH0_DOMAIN=%AUTH0_DOMAIN% -a raterware
+
+echo Setting AUTH0_CALLBACK_URL on Heroku
 heroku config:set AUTH0_CALLBACK_URL=%AUTH0_CALLBACK_URL% -a raterware
+
+echo Setting STRIPE_SECRET_KEY on Heroku
 heroku config:set STRIPE_SECRET_KEY=%STRIPE_SECRET_KEY% -a raterware
+
+echo Setting STRIPE_PUBLISHABLE_KEY on Heroku
 heroku config:set STRIPE_PUBLISHABLE_KEY=%STRIPE_PUBLISHABLE_KEY% -a raterware
 
 echo Environment variables set successfully on Heroku.
