ba81d66a8b647b5fa4ac1d791f7b09e3b9370c38
Rater <support@raterware.com>
2024-07-12 00:08:53 +0200

    Nullable username i models

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class Client(db.Model):
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=True)  # Make nullable
     is_admin = db.Column(db.Boolean, default=False)
